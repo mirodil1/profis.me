597f7903d44b3ab57487b1248e1ebeee45022fc3
mirodil <ortikov.m97@gmail.com>
2023-09-21 18:37:33 +0500

    imports

---
+++ profis/tasks/views.py
@@ -0,0 +1,5 @@
+# from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+# from rest_framework.viewsets import GenericViewSet
+
+# from profis.tasks.models import Task, TaskImage
+# from profis.tasks.serializers import TaskCreateSerializer, TaskSerializer
