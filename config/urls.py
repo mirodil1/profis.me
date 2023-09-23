8d1a41c4f47d29423cb82eca51f9594e5c0af693
mirodil <ortikov.m97@gmail.com>
2023-09-23 16:16:05 +0500

    api urls

--- config/urls.py
+++ config/urls.py
@@ -9,14 +9,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("profis.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
@@ -25,7 +20,7 @@ if settings.DEBUG:
 # API URLS
 urlpatterns += [
     # API base url
-    path("api/", include("config.api_router")),
+    path("api/users/", include("profis.users.urls", namespace="users")),
     path("api/categories/", include("profis.categories.urls", namespace="categories")),
     path("api/tasks/", include("profis.tasks.urls", namespace="tasks")),
     # DRF auth token
