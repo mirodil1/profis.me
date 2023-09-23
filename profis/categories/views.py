eee6ef272a2940b44e222a8d1f4fb86c812477b5
mirodil <ortikov.m97@gmail.com>
2023-09-23 16:15:43 +0500

    extend_scheme

--- profis/categories/views.py
+++ profis/categories/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
@@ -5,6 +6,7 @@ from profis.categories.models import Category
 from profis.categories.serializers import CategorySerializer
 
 
+@extend_schema(tags=["categories"])
 class CategoryListViewSet(ListModelMixin, GenericViewSet):
     queryset = Category.objects.filter(parent_category=None)
     serializer_class = CategorySerializer
