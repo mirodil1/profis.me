ddfd3a8a80d811e213988164aa829f3fc1ee77d9
mirodil <ortikov.m97@gmail.com>
2023-09-23 16:11:38 +0500

    user serializer

--- profis/users/serializers.py
+++ profis/users/serializers.py
@@ -7,6 +7,37 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer[UserType]):
+    avatar = serializers.ImageField(read_only=True)
+    age = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = User
-        fields = ["first_name", "last_name"]
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "bio",
+            "age",
+            "number_of_views",
+            "is_worker",
+            "avatar",
+            "work_experiance",
+            "gender",
+        ]
+
+
+class UserCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "bio",
+            "date_of_birth",
+            "avatar",
+            "is_worker",
+            "work_experiance",
+            "gender",
+        ]
