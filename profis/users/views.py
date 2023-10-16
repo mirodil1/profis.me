fbc438b742118d32943715b47b601c80f0db9344
mirodil <ortikov.m97@gmail.com>
2023-10-16 12:06:14 +0500

    google oauth2

--- profis/users/views.py
+++ profis/users/views.py
@@ -1,10 +1,16 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
+from django.views.generic import RedirectView
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from profis.users.serializers import UserSerializer
@@ -36,5 +42,29 @@ class WorkerListViewSet(ListModelMixin, GenericViewSet):
     def get_queryset(self, *args, **kwargs) -> QuerySet:
         if "category_slug" in self.kwargs:
             category = self.kwargs["category_slug"]
-            return self.queryset.filter(categories__slug=category)
+            return self.queryset.filter(is_worker=True, categories__slug=category)
         return self.queryset.filter(is_worker=True)
+
+
+@extend_schema(tags=["auth"])
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://127.0.0.1:8000/api/users/"
+    client_class = OAuth2Client
+
+
+@extend_schema(tags=["auth"])
+class SendSMSAPIView(APIView):
+    def post(self, request):
+        pass
+
+
+class UserRedirectView(LoginRequiredMixin, RedirectView):
+    """
+    This view is needed by the dj-rest-auth-library in order to work the google login. It's a bug.
+    """
+
+    permanent = False
+
+    def get_redirect_url(self):
+        return "redirect-url"
