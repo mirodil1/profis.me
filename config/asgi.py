91e67448d8c2711c4c3bee2007425a6ec77a69b6
mirodil <ortikov.m97@gmail.com>
2023-11-08 16:01:37 +0500

    websocket config

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,9 @@ import os
 import sys
 from pathlib import Path
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 # This allows easy placement of apps within the interior
@@ -28,13 +31,21 @@ django_application = get_asgi_application()
 # application = HelloWorldApplication(application)
 
 # Import websocket application here, so apps from django_application are loaded first
-from config.websocket import websocket_application  # noqa isort:skip
+# from config.websocket import websocket_application  # noqa isort:skip
+from profis.chats.routing import websocket_urlpatterns  # noqa isort:skip
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_application,
+        "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+    }
+)
 
 
-async def application(scope, receive, send):
-    if scope["type"] == "http":
-        await django_application(scope, receive, send)
-    elif scope["type"] == "websocket":
-        await websocket_application(scope, receive, send)
-    else:
-        raise NotImplementedError(f"Unknown scope type {scope['type']}")
+# async def application(scope, receive, send):
+#     if scope["type"] == "http":
+#         await django_application(scope, receive, send)
+#     elif scope["type"] == "websocket":
+#         await websocket_application(scope, receive, send)
+#     else:
+#         raise NotImplementedError(f"Unknown scope type {scope['type']}")
