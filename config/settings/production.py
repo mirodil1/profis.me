bfcf7c02defc8d83d7b25009cda655c44a384ba8
mirodil <ortikov.m97@gmail.com>
2023-12-18 17:12:05 +0500

    static fiel storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -56,9 +56,9 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+    # "staticfiles": {
+    #     "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    # },
 }
 # MEDIA
 # ------------------------------------------------------------------------------
